b2081e7249dc520e16f5ab86445a649adae1a349
ClassicRagu <megameowthethird@gmail.com>
2025-03-17 21:05:16 -0500

    Update packages and fix some mobile formatting

--- src/components/shared/GeneratedSidebar.tsx
+++ src/components/shared/GeneratedSidebar.tsx
@@ -163,8 +163,8 @@ export default function GeneratedSidebar(props: {
         component="main"
         sx={{
           flexGrow: 1,
-          p: 3,
-          width: { sm: `calc(100% - ${drawerWidth}px)` },
+          pt: 3,
+          width: {xs:"100%" , sm: `calc(100% - ${drawerWidth}px)` },
         }}
       >
         {props.children}
