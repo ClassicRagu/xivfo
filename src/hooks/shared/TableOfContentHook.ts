9cb3198a70fd387385297967ec17e81cb8a636f5
ClassicRagu <megameowthethird@gmail.com>
2025-04-17 21:47:29 -0500

    Change the final js file to ts

--- src/hooks/shared/TableOfContentHook.ts
+++ src/hooks/shared/TableOfContentHook.ts
@@ -4,22 +4,21 @@ import { useEffect, useState, useRef } from 'react';
 export function useHeadsObserver( ) {
 
   const [activeId, setActiveId] = useState('')
-  const observer = useRef()
+  
+  const handleObsever = (entries: IntersectionObserverEntry[]) => {
+    console.log(entries)
+    entries.forEach((entry) => {
+      if (entry?.isIntersecting) {
+        setActiveId(entry.target.id)
+      }
+    })
+  }
+
+  const observer = useRef(new IntersectionObserver(handleObsever, {
+    rootMargin: "-20% 0% -35% 0px"}
+  ))
 
   useEffect(() => {
-
-    const handleObsever = (entries) => {
-      entries.forEach((entry) => {
-        if (entry?.isIntersecting) {
-          setActiveId(entry.target.id)
-        }
-      })
-    }
-
-    observer.current = new IntersectionObserver(handleObsever, {
-      rootMargin: "-20% 0% -35% 0px"}
-    )
-    
     const elements = document.querySelectorAll("h2, h3")
     elements.forEach((elem) => observer.current.observe(elem))
     return () => observer.current?.disconnect()
