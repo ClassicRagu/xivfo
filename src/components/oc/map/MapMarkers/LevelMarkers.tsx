5a4bfc2e75eae3a542c11954700da0aa9df16e82
ClassicRagu <megameowthethird@gmail.com>
2025-06-23 23:54:43 -0500

    1 more level zone remaining

--- src/components/oc/map/MapMarkers/LevelMarkers.tsx
+++ src/components/oc/map/MapMarkers/LevelMarkers.tsx
@@ -1,4 +1,3 @@
-import { setColor } from "@/functions/bozja/Farming/Fragment_Map/setColor";
 import { levelzones } from "@/static/oc/map/levelzones";
 import { MonsterLocation } from "@/types/bozja/Farming/Fragment_Map/MonsterLocation";
 import { Icon, LatLngExpression, LatLngTuple } from "leaflet";
@@ -7,6 +6,25 @@ import { Circle, Marker, Polygon, Popup } from "react-leaflet";
 function mapXY(x:number, y: number) : LatLngTuple {
   return [42.9 - y, x];
 }
+
+const setColor = (level: number | string) => {
+    switch (level) {
+      case 1:
+        return "green";
+      case 2:
+        return "yellow";
+      case 3:
+        return "red";
+      case 4:
+        return "purple";
+      case 5:
+        return "blue";
+      case "Critical Engagement":
+        return "white";
+      default:
+        return "black";
+    }
+  };
 
 export function MapMarkers(props: {
   minInputLevel: number;
@@ -25,8 +43,8 @@ export function MapMarkers(props: {
         <Polygon
           key={`${index}-${levelIndex}`}
           pathOptions={{
-            fillColor: setColor(index % 5),
-            color: setColor(index % 5),
+            fillColor: setColor(index % 6),
+            color: setColor(index % 6),
           }}
           positions={positions}
         />
